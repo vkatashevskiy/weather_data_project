7fdb065c5e5512e6eee07ccd47fbabe0a504893f
vkatashevskiy <v.katashevskiy@gmail.com>
2026-06-10 00:45:43 +0300

    еще добавлены комменты

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
 # главный файл, в котором есть консольное меню
+# ПЕРЕД ТЕМ КАК НАЧИНАТЬ РАБОТУ:
+# git pull origin main
+#
+# ПОСЛЕ ТОГО КАК НАПИСАЛА КОД:
+# git status
+# git add .
+# git commit -m "ТВОЙ КОММЕНТ"
+# git push origin main
