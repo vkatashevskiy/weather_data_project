db7679d195b6bf0e893da370b3c5eaef13eb92b6
mimzonka1 <mimzonka@gmail.com>
2026-06-11 00:48:31 +0300

    в модуле io_handler добавлена загрузка файла, в модуле analyzer строится массив префиксных сумм, находится средняя температура для каждого месяца, находится самый теплый день, самый холодный день, сортировка месяцев по средней температуре

--- analyzer.py
+++ analyzer.py
@@ -1,1 +1,115 @@
 # префиксные суммы, поиск максимума и минимума, сортировка
+def build_prefix_sums(weather_data):
+    """
+    Принимает список словарей с данными о погоде
+    и возвращает массив префиксных сумм температур.
+    """
+    if not weather_data: # Если данных нет, возвращаем [0]
+        return [0]
+
+    prefix_sums = [0] # Первый элемент равен 0, для упрощения дальнейших расчетов
+
+    current_sum = 0
+    for record in weather_data:
+        current_sum += record["temperature"] # Достаем температуру из словаря и прибавляем к текущей сумме
+        prefix_sums.append(current_sum)
+
+    return prefix_sums
+
+
+def average_by_month(weather_data, prefix_sums):
+    """
+    Вычисляет среднюю температуру для каждого месяца,
+    используя массив префиксных сумм.
+    """
+    if not weather_data or len(prefix_sums) != len(weather_data) + 1:
+        return {}
+
+    months_indices = {} # Группируем индексы дней по месяцам
+    for index, record in enumerate(weather_data):
+        month = record["month"]
+        if month not in months_indices:
+            months_indices[month] = []
+        months_indices[month].append(index)
+
+    monthly_averages = {}
+
+    for month, indices in months_indices.items(): # Считаем среднее для каждого месяца через префиксные суммы
+        start_idx = indices[0] # Индекс первого и последнего дня месяца в исходном списке
+        end_idx = indices[-1]
+
+        days_count = len(indices) # Количество дней в месяце
+
+        month_sum = prefix_sums[end_idx + 1] - prefix_sums[start_idx] # Формула префиксных сумм
+
+        monthly_averages[month] = round(month_sum / days_count, 2) # Считаем среднее и округляем до 2 знаков после запятой
+
+    return monthly_averages
+
+
+def find_max_temperature(weather_data):
+    """
+    Линейным поиском определяет самый тёплый день.
+    Возвращает кортеж: (дата в формате ДД.ММ.ГГГГ, температура)
+    """
+    if not weather_data:
+        return None  # Если данных нет, возвращаем ничего
+
+    max_record = weather_data[0] # Инициализируем начальное максимальное значение первым элементом списка
+
+    for record in weather_data[1:]: # Линейный поиск: перебираем все элементы со второго до последнего
+        if record["temperature"] > max_record["temperature"]:
+            max_record = record
+
+    date_str = f"{max_record['day']:02d}.{max_record['month']:02d}.{max_record['year']}"
+    max_temp = max_record["temperature"]
+
+    return date_str, max_temp
+
+
+def find_min_temperature(weather_data):
+    """
+    Линейным поиском определяет самый холодный день.
+    Возвращает кортеж: (дата в формате ДД.ММ.ГГГГ, температура)
+    """
+    if not weather_data:
+        return None  # Если данных нет, возвращаем ничего
+
+    min_record = weather_data[0] # Инициализируем начальное минимальное значение первым элементом списка
+
+    for record in weather_data[1:]: # Линейный поиск: перебираем все элементы со второго до последнего
+        if record["temperature"] < min_record["temperature"]:
+            min_record = record
+
+    date_str = f"{min_record['day']:02d}.{min_record['month']:02d}.{min_record['year']}"
+    min_temp = min_record["temperature"]
+
+    return date_str, min_temp
+
+
+def sort_months(monthly_averages):
+    """
+    Сортирует месяцы по средней температуре.
+    Возвращает новый отсортированный словарь.
+    """
+    items = list(monthly_averages.items()) # Превращаем словарь в список кортежей вида [(месяц, температура), ...]
+    n = len(items)
+
+    def quick_sort(items):
+        if len(items) < 1:
+            return items
+        else:
+            low = items[0]
+            high = items[len(items) - 1]
+            demi = items[len(items) // 2]
+            pivot = np.median([low[2], high[2], demi[2]])
+
+            left = [i for i in items if i[2] < pivot]
+            equal = [k for k in items if k[2] == pivot]
+            right = [j for j in items if j[2] > pivot]
+
+         return quick_sort(left) + equal + quick_sort(right)
+
+    # Превращаем отсортированный список кортежей обратно в словарь
+    sorted_months_dict = quick_sort(items)
+    return sorted_months_dict
--- io_handler.py
+++ io_handler.py
@@ -1,1 +1,57 @@
 # файл читает csv
+import csv
+import os
+
+
+def read_csv(filename):
+    if not os.path.exists(filename): # Проверяем существует ли файл
+        raise FileNotFoundError(f"Файл '{filename}' не найден.")
+
+    data = []
+
+    with open(filename, mode='r', encoding='utf-8') as file:
+        header = file.readline().strip().split(',') # Читаем первую строку (заголовки), чтобы проверить формат
+        expected_header = ['year', 'month', 'day', 'temperature']
+
+        if header != expected_header: # Проверяем корректен ли формат (заголовки)
+            raise ValueError(f"Некорректный формат CSV. Ожидались заголовки: {expected_header}")
+
+        file.seek(0) # Возвращаем указатель в начало файла и пропускаем заголовок для csv.Reader
+        next(file)
+
+        for line_number, line in enumerate(file, start=2): # Читаем остальные строки
+            stripped_line = line.strip()
+
+            if not stripped_line: # Проверяем отсутствуют ли пустые строки
+                raise ValueError(f"Ошибка: обнаружена пустая строка на позиции {line_number}.")
+
+            reader = csv.reader([stripped_line]) # Разбираем строку через csv.reader, чтобы правильно обработать разделители
+            row = next(reader)
+
+            if len(row) != 4: # Проверяем, что в строке ровно 4 элемента
+                raise ValueError(f"Ошибка в строке {line_number}: неверное количество колонок.")
+
+            try: # Преобразуем значения в числа
+                row_dict = {
+                    "year": int(row[0]),
+                    "month": int(row[1]),
+                    "day": int(row[2]),
+                    "temperature": int(row[3])
+                }
+                data.append(row_dict)
+            except ValueError:
+                raise ValueError(f"Ошибка в строке {line_number}: данные должны быть числами.")
+
+    return data
+
+
+if __name__ == "__main__":
+    name_of_file = "weather.csv"
+
+    try:
+        weather_data = read_csv(name_of_file)  # Вызываем функцию и сохраняем результат в переменную
+
+        print("Данные успешно прочитаны:") # Красиво выводим результат в консоль
+
+    except Exception as e:
+        print(f"Произошла ошибка при чтении: {e}") # Если какая-то проверка не пройдется, мы увидим ошибку здесь
